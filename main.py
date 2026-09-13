78229cf47157ca4320c849d6145d891464b50c86
Vladimir <vladimir.lima.1950@gmail.com>
2026-09-13 08:13:09 -0300

    melhorada a rota de acesso

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import sys
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(BASE_DIR, "modules"))
+
+
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
 import os
